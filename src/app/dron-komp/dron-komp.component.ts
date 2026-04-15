b7d152c0c4ed73c7e830082b9007dd3eba6abd89
chaosRobe <dominik.panasz@outlook.com>
2026-04-15 13:35:19 +0200

    do 7

--- src/app/dron-komp/dron-komp.component.ts
+++ src/app/dron-komp/dron-komp.component.ts
@@ -10,10 +10,20 @@ export class DronKompComponent {
   @Input() licz: number = 0;
   @Input() dronNazwa: string = '';
   energia: number = 0;
-
+  currentStyles: Record<string, string> = {};
   onEnergiaClick(): void{
     if(this.energia < 100){
       this.energia = this.energia + 5;
     }
   }
+  onMouseEntr(): void{
+    this.currentStyles = {
+      'font-size': '23px'
+  }
+}
+  onMouseL(): void{
+    this.currentStyles = {
+      'font-size': '16px'
+  }
+}
 }
